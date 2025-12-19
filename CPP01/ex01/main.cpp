7ceba63e519d0cb55f57e4a68bbf715d983f0935
Aymane Kahir <akahir@e2r9p11.1337.ma>
2025-12-19 18:10:45 +0100

    Done

--- CPP01/ex01/main.cpp
+++ CPP01/ex01/main.cpp
@@ -4,10 +4,10 @@ Zombie* zombieHorde(int N, std::string name);
 
 int main()
 {
-    int N = 214748;
+    int N = 5;
     if (N < 0 || N > 2147483647)
         return (1);
-    Zombie *zombie = zombieHorde(N, "aymane");
+    Zombie *zombie = zombieHorde(N, "akahir");
     if (!zombie)
         return (1);
     for (int i = 0; i < N; i++)
